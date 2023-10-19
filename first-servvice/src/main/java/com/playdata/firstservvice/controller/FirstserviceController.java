1be06a3ca2da155e9dd82f8f246351f42bfa4c13
Hwater00 <khwater1017@naver.com>
2023-10-19 17:02:34 +0900

    요청헤더에 든 값 컨트롤러로 감지하기

--- first-servvice/src/main/java/com/playdata/firstservvice/controller/FirstserviceController.java
+++ first-servvice/src/main/java/com/playdata/firstservvice/controller/FirstserviceController.java
@@ -1,8 +1,6 @@
 package com.playdata.firstservvice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/first-service")
@@ -10,5 +8,11 @@ public class FirstserviceController {
     @RequestMapping(value="hello",method = RequestMethod.GET)
     public String hello(){
         return "Hello, First-service";
+    }
+
+
+    @GetMapping("header-check")
+    public String headerCheck(@RequestHeader("fsreqh") String header){
+        return header;
     }
 }
