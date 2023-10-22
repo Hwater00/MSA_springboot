1e9c379b3ad4371d183de9ebf7945c9e583b6d36
Hwater00 <khwater1017@naver.com>
2023-10-22 17:50:36 +0900

    람다식 없고 OrderedGatewayFilter를 이용한 우선순위 설정

--- apigateway-server/src/main/java/com/example/apigatewayserver/filter/CustomFilter2.java
+++ apigateway-server/src/main/java/com/example/apigatewayserver/filter/CustomFilter2.java
@@ -1,7 +1,9 @@
 package com.example.apigatewayserver.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -27,6 +29,16 @@ public class CustomFilter2 extends AbstractGatewayFilterFactory<CustomFilter2.Co
             }));
         };
     }
+
+//    // 인터페이스인 GatewayFilter를 구현한 구현체 OrderedGatewayFilter를 이용한 우선순위 조정
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//       GatewayFilter gatewayFilter = new OrderedGatewayFilter((exchange, chain) -> {
+//
+//           // 숫자가 작을수록 우선순위가 높다 = 우선순위:100> 1000
+//       }, Ordered.LOWEST_PRECEDENCE); // 필터의 우선순위 MAX_Value 가장 낮은 우선순위를 의미
+//    return gatewayFilter;
+//    }
 
 
 
